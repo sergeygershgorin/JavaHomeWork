4099274de25b77e79dfb025a188a61de5684cc21
Sergey Gershgorin <gershgorin@gmail.com>
2021-10-24 21:35:32 +0300

    init commit

---
+++ src/com/pb/gershgorin/hw3/array.java
@@ -0,0 +1,7 @@
+package com.pb.gershgorin.hw3;
+
+public class array {
+    public static void main(String[] args) {
+
+    }
+}
--- src/com/pb/gershgorin/hw3/bingo.java
+++ src/com/pb/gershgorin/hw3/bingo.java
@@ -1,11 +1,14 @@
 package com.pb.gershgorin.hw3;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class bingo {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int operand1 = 11;
+        //int operand1 = 11;
+        Random random = new Random();
+        int operand1= random.nextInt(101);
         int input;
         int attempt = 0;
         System.out.println("Пожалуйста, угадайте число от 0 до 100, для выхода из игры введите число больше 100:");
