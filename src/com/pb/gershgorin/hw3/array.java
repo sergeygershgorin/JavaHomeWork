7b3b19c20a1104de51eb4728f7570b072d7fd082
Sergey Gershgorin <gershgorin@gmail.com>
2021-10-24 23:14:56 +0300

    init commit

--- src/com/pb/gershgorin/hw3/array.java
+++ src/com/pb/gershgorin/hw3/array.java
@@ -8,6 +8,8 @@ public class array {
         int[] array = new int[5];
         int sum = 0;
         int positiv = 0;
+        int temp;
+        boolean babbles = false;
 
         System.out.println("Введите 10 элементов массива");
         for (int i = 0; i < array.length; i++) {
@@ -28,5 +30,21 @@ public class array {
             if (array[i]>0){positiv++;};
         }
         System.out.println("Количество положительных элементов массива:" + positiv);
+
+        while(!babbles) {
+            babbles = true;
+            for (int i = 0; i < array.length-1; i++) {
+                if(array[i] > array[i+1]){
+                    babbles = false;
+                    temp = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = temp;
+                }
+            }
+        }
+        System.out.println("Отсортированный по возрастанию массив:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
 }
